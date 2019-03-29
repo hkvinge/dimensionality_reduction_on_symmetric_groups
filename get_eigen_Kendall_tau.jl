e1afc6d0796bda9e5ddae894a6cbd298f52a8ce2
hkvinge <henry.kvinge@colostate.edu>
2019-03-29 14:12:55 -0600

    Refined output.

--- get_eigen_Kendall_tau.jl
+++ get_eigen_Kendall_tau.jl
@@ -1,20 +1,24 @@
 using SnFFT
 
+# Choose the size of symmetric group to compute eigenvalues for
 n = 5
+
+# If print_perm is equal to 1, then all permutations and their FFT
+# are printed out (if n > 6, do not do this).
+print_perm = 0
+
+# If print_embed is equal to 1, then he MDS coordinates of each
+# permutation are printed out (if n > 6, do not do this).
+print_embed = 0
 
 # Generate permutations
 perms = permutations(1:n) |> collect
+
+# Set array to hold matrix coefficients for FFT
 record_transform = zeros(factorial(n),factorial(n))
 
+# Loop through all permutations of S_n, computing FFT of each
 for i = 1:factorial(n)
-    
-    # Print permutation
-    println("                  ")
-    println("                  ")
-    println("The decomposition for permutation ")
-    println(perms[i])
-    println("is")
-    println("******************")
 
     # Set proper coefficient equal to 1
     perm_coef = zeros(factorial(n))
@@ -29,8 +33,18 @@ for i = 1:factorial(n)
     # Get Fourier transform
     x_trans = sn_fft(n,x,RA,PT)
 
-    println(x_trans)
- 
+    # If print_perm is equal to 1, print each permutation and its
+    # FFT
+    if (print_perm == 1)
+        println("                  ")
+        println("                  ")
+        println("The decomposition for permutation ")
+        println(perms[i])
+        println("is")
+        println("******************")
+        println(x_trans)
+    end
+
     # Record the transform in one matrix. This will be used to 
     # find which eigenvalues of the MDS operator correspond to
     # which representations   
@@ -46,20 +60,17 @@ for i = 1:factorial(n)
             for l = 1:rep_size[1]
                 record_transform[i,count] = rep[k,l]
                 count = count + 1
-        
             end
         end
     end
 end
 
-# Get coordinates of identity element to center embedding
-
-
+# Create matrix to store Kendall tau distances
 distance_matrix = zeros(factorial(n),factorial(n))
 
 # Compute the pairwise Kendall tau distance between all points
+# and store these in the distance matrix
 for i = 1:factorial(n)
-    println(i)
     for j = 1:factorial(n)
         p1 = preferencematrix(perms[i])
         p2 = preferencematrix(perms[j])
@@ -67,7 +78,7 @@ for i = 1:factorial(n)
     end
 end
 
-# Perform MDS
+# Perform MDS on the distance matrix that has been found
 D = map(x->.5*x^2,distance_matrix)
 H = eye(factorial(n),factorial(n)) - (1/factorial(n))*ones(factorial(n),factorial(n))
 M = H*D*H
@@ -75,23 +86,32 @@ M = H*D*H
 U,S,V = svd(M)
 U = map(x->round(x,3),U)
 S = map(x->round(x,6),S)
+
+# Print eigenvalues for MDS operator
+println("The full list of MDS eigenvalues is")
 println(S)
-for i = 1:factorial(n)
-    println("The coordinates of")
-    println(perms[i])
-    println("is")
-    println(U[i,:])
-    println("----------------")
+
+# Print coordinate
+if print_embed == 1
+    for i = 1:factorial(n)
+        println("The coordinates of")
+        println(perms[i])
+        println("is")
+        println(U[i,:])
+        println("----------------")
+    end
 end
 
-# Solving linear systems to determine which eigenvalue corresponds
+# Solve the linear system to determine which eigenvalue corresponds
 # to each representation
-
 for i = 1:factorial(n)
-    x = record_transform\U[:,i]
-    println("This eigenvalue corresponds to")
-    println(i)
-    println(S[i])
-    println(round(x,3))
-    println("------------------------------")
+    # Only print this data when the eigenvalue is not zero
+    if S[i] != 0.0
+        x = record_transform\U[:,i]
+        println("The eigenvalue")
+        println(S[i])
+        println("corresponds to coefficients:")
+        println(round(x,3))
+        println("------------------------------")
+    end
 end
